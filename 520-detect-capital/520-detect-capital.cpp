1a5635688a44af71c7de15da34cb405bacafc2bb
Eomy <hmu1212@hotmail.com>
2022-01-30 11:58:36 +0900

    Time: 3 ms (47.60%), Space: 6.1 MB (68.02%) - LeetHub

---
+++ 520-detect-capital/520-detect-capital.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        if(word.length() == 1) return true;
+        
+        if(word[0] >= 'A' && word[0] <= 'Z') {
+            if(word[1] < 'A' || word[1] > 'Z') {
+                for(int i = 2; i < word.length(); ++i)
+                    if(word[i] >= 'A' && word[i] <= 'Z') return false;
+            } else {
+                for(int i = 2; i < word.length(); ++i)
+                    if(word[i] < 'A' || word[i] > 'Z') return false;
+            }
+        } else {
+            for(int i = 1; i < word.length(); ++i) {
+                if(word[i] >= 'A' && word[i] <= 'Z') return false;
+            }
+        }
+        
+        
+        return true;
+    }
+};

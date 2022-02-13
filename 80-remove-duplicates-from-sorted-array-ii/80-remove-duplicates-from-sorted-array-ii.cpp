925e99ed66c88ac9dbabe92fa7908ac82d9f3247
Eomy <hmu1212@hotmail.com>
2022-02-13 16:31:19 +0900

    Time: 14 ms (17.86%), Space: 11.6 MB (7.07%) - LeetHub

--- 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int ret = 0;
-        
         unordered_map<int, int> um;
         for(int i = 0; i < nums.size(); ) {
             if(um[nums[i]] >= 2) {
@@ -10,10 +8,9 @@ public:
                 continue;
             }
             um[nums[i]]++;
-            ret++;
             i++;
         }
         
-        return ret;
+        return nums.size();
     }
 };
